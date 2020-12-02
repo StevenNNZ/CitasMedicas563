65a1e5c84c8c3033015e14fdc877762903f24fb2
Beronica01 <Beronica01>
2020-12-02 13:28:13 -0500

    prueba-commit

--- src/Controlador/prueba.java
+++ src/Controlador/prueba.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package Controlador;
 
 /**
@@ -11,4 +8,6 @@ package Controlador;
  */
 public class prueba {
     
+    
+    //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 }
